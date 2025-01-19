f18fae620b9fa6bbdbf66f54ec85e64f2395d317
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-01-19 19:56:31 +0530

    update package import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:home_bites/presentation/screens/home_screen.dart';
+import 'package:home_bites/presentation/screens/Home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
