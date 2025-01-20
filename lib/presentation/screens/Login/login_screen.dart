8550b99869207924d87acff04027f675446cc3bd
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-01-20 12:34:02 +0530

    implement basic login screen layout

--- lib/presentation/screens/Login/login_screen.dart
+++ lib/presentation/screens/Login/login_screen.dart
@@ -8,7 +8,48 @@ class LoginScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: Center(
-      child: Text("This is Login screen"),
+      //login form
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            width: size.width * 0.8,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Email',
+                  icon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            width: size.width * 0.8,
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  icon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            width: size.width * 0.8,
+            child: ElevatedButton(
+              onPressed: () {
+                //login action
+              },
+              child: Text('Login'),
+            ),
+          ),
+        ],
+      ),
     ));
   }
 }
