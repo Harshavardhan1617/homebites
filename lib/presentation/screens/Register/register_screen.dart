56a00939198913f938942e7ae46f6136782893f3
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-01-20 14:25:05 +0530

    add basic layout for register screen

--- lib/presentation/screens/Register/register_screen.dart
+++ lib/presentation/screens/Register/register_screen.dart
@@ -8,7 +8,48 @@ class RegisterScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: Center(
-      child: Text("This is Register screen"),
+      //login form
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            width: size.width * 0.8,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Mobile Number',
+                  icon: Icon(Icons.phone),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            width: size.width * 0.8,
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  icon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            width: size.width * 0.8,
+            child: ElevatedButton(
+              onPressed: () {
+                //login action
+              },
+              child: Text('Register'),
+            ),
+          ),
+        ],
+      ),
     ));
   }
 }
