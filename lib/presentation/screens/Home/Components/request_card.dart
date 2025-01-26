594b89541fc5c2dd17140df134b2474ec49306d5
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-01-26 19:29:43 +0530

    fix: placeholder profile pic

--- lib/presentation/screens/Home/Components/request_card.dart
+++ lib/presentation/screens/Home/Components/request_card.dart
@@ -13,11 +13,13 @@ class ReqCard extends StatelessWidget {
     return Card(
       child: ListTile(
         leading: CircleAvatar(
-          backgroundImage: user['avatar'] != null
+          backgroundImage: user['avatar'] != null && user['avatar']!.isNotEmpty
               ? NetworkImage(
                   'http://127.0.0.1:8090/api/files/${user['collectionId']}/${user['id']}/${user['avatar']}')
               : null,
-          child: user['avatar'] == null ? Icon(Icons.person) : null,
+          child: (user['avatar'] == null || user['avatar']!.isEmpty)
+              ? Icon(Icons.person)
+              : null,
         ),
         title: Text(user['name']),
         subtitle: Column(
