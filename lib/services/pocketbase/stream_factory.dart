36055f2b99ff6e58a6cbbe93b2be88df3457a100
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-01-27 21:42:56 +0530

    add stream factory to seperate stream controller

---
+++ lib/services/pocketbase/stream_factory.dart
@@ -0,0 +1,16 @@
+import 'package:home_bites/services/pocketbase/stream.dart';
+import 'package:pocketbase/pocketbase.dart';
+
+class RequestsStreamControllerFactory {
+  static RequestsStreamController? _instance;
+
+  static RequestsStreamController getInstance(PocketBase pb) {
+    _instance ??= RequestsStreamController(pb: pb);
+    return _instance!;
+  }
+
+  static void dispose() {
+    _instance?.dispose();
+    _instance = null;
+  }
+}
