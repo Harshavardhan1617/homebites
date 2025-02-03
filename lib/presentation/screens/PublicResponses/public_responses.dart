29b0278e7966c3fc18a69fbe43bdda806aeded01
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-02-03 17:56:04 +0530

    add stream builder to public responses

--- lib/presentation/screens/PublicResponses/public_responses.dart
+++ lib/presentation/screens/PublicResponses/public_responses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_bites/presentation/screens/Forms/responses_form.dart';
 import 'package:home_bites/services/pocketbase/pbase.dart';
+import 'package:home_bites/services/pocketbase/responses_stream.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:provider/provider.dart';
 
@@ -13,10 +14,24 @@ class PublicResponses extends StatefulWidget {
 }
 
 class _PublicResponsesState extends State<PublicResponses> {
-  Future<ResultList<RecordModel>> _fetchResponses() async {
-    final PocketBaseService pb =
+  late ResponsesStream _responsesStream;
+
+  @override
+  void initState() {
+    super.initState();
+    final PocketBaseService pbService =
         Provider.of<PocketBaseService>(context, listen: false);
-    return pb.getResponses(widget.requestID);
+    _responsesStream = ResponsesStream(pb: pbService.pb);
+  }
+
+  @override
+  void dispose() {
+    _responsesStream.dispose();
+    super.dispose();
+  }
+
+  Future<void> _onRefresh() async {
+    await _responsesStream.refreshData();
   }
 
   @override
@@ -25,32 +40,34 @@ class _PublicResponsesState extends State<PublicResponses> {
       appBar: AppBar(
         title: const Text('Public Responses'),
       ),
-      body: FutureBuilder<ResultList<RecordModel>>(
-        future: _fetchResponses(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.items.length,
-              itemBuilder: (context, index) {
-                final response = snapshot.data!.items[index].data;
-                return ListTile(title: Text(response['note']));
-              },
-            );
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
+      body: RefreshIndicator(
+        onRefresh: _onRefresh,
+        child: StreamBuilder<List<RecordModel>>(
+          stream: _responsesStream.responseStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final response = snapshot.data![index].data;
+                  return ListTile(title: Text(response['note']));
+                },
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) {
-                return ResponsesFormScreen(requestID: widget.requestID);
-              },
+              builder: (context) =>
+                  ResponsesFormScreen(requestID: widget.requestID),
             ),
           );
         },
