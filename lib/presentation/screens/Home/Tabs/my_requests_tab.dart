57b64185f32065a65dc8231eaa758eb4df486b02
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-02-03 23:06:13 +0530

    integerate provider to limit my requests

--- lib/presentation/screens/Home/Tabs/my_requests_tab.dart
+++ lib/presentation/screens/Home/Tabs/my_requests_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_bites/presentation/providers/vars_provider.dart';
 import 'package:home_bites/presentation/screens/Home/Components/request_card.dart';
 import 'package:home_bites/services/pocketbase/pbase.dart';
 import 'package:pocketbase/pocketbase.dart';
@@ -13,6 +14,8 @@ class MyRequestsTab extends StatefulWidget {
 
 class _MyRequestsTabState extends State<MyRequestsTab> {
   late Future<ResultList<RecordModel>> _fetchedRequests;
+  bool _isLoading = false;
+  String _errorMessage = '';
 
   @override
   void initState() {
@@ -21,37 +24,60 @@ class _MyRequestsTabState extends State<MyRequestsTab> {
   }
 
   Future<void> _fetchData() async {
-    final PocketBaseService pbService =
-        Provider.of<PocketBaseService>(context, listen: false);
-    _fetchedRequests =
-        pbService.getMyRequests(pbService.pb.authStore.record!.id);
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    try {
+      final PocketBaseService pbService =
+          Provider.of<PocketBaseService>(context, listen: false);
+      _fetchedRequests =
+          pbService.getMyRequests(pbService.pb.authStore.record!.id);
+
+      // Update the int provider after the data is fetched
+      final requestsCount =
+          await _fetchedRequests.then((result) => result.totalItems);
+      final intProvider = Provider.of<MyIntProvider>(context, listen: false);
+      intProvider.updateInt(requestsCount);
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Error fetching requests: $e';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
       onRefresh: _fetchData,
-      child: FutureBuilder<ResultList<RecordModel>>(
-        future: _fetchedRequests,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData) {
-            final requests = snapshot.data!;
-            return ListView.builder(
-              itemCount: requests.totalItems,
-              itemBuilder: (context, index) {
-                final request = requests.items[index];
-                return ReqCard(request: request);
-              },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      child: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _errorMessage.isNotEmpty
+              ? Center(child: Text(_errorMessage))
+              : FutureBuilder<ResultList<RecordModel>>(
+                  future: _fetchedRequests,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final requests = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: requests.totalItems,
+                        itemBuilder: (context, index) {
+                          final request = requests.items[index];
+                          return ReqCard(request: request);
+                        },
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
     );
   }
 }
--- lib/presentation/screens/Home/home_screen.dart
+++ lib/presentation/screens/Home/home_screen.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:home_bites/presentation/providers/vars_provider.dart';
+import 'package:home_bites/presentation/screens/Forms/requests_form.dart';
 import 'package:home_bites/presentation/screens/Home/Tabs/requests_tab.dart';
 import 'package:home_bites/presentation/screens/Home/Tabs/my_requests_tab.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-          appBar: AppBar(
-            title: Text('Home Bites'),
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Requests'),
-                Tab(text: 'My Requests'),
-                // Tab(text: 'Offerings'),
+            appBar: AppBar(
+              title: Text('Home Bites'),
+              bottom: TabBar(
+                onTap: (value) => setState(() => _selectedIndex = value),
+                tabs: [
+                  Tab(text: 'Requests'),
+                  Tab(text: 'My Requests'),
+                  // Tab(text: 'Offerings'),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                RequestsTab(),
+                MyRequestsTab()
+                // ProfileTab(),
               ],
             ),
-          ),
-          body: TabBarView(
-            children: [
-              RequestsTab(),
-              MyRequestsTab()
-              // ProfileTab(),
-            ],
-          ),
-        ));
+            floatingActionButton: _selectedIndex == 1 &&
+                    context.watch<MyIntProvider>().myInt <= 1
+                ? FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RequestsForm(),
+                          ));
+                    },
+                    child: const Icon(Icons.add),
+                  )
+                : null)); // Important: Return null if the condition is false));
   }
 }
