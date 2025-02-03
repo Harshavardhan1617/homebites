ffcc1c994a3b83fc3aeeb9f1febbb7f8a369a69a
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-02-03 21:02:25 +0530

    remove add requests frm requests tab

--- lib/presentation/screens/Home/Tabs/requests_tab.dart
+++ lib/presentation/screens/Home/Tabs/requests_tab.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:home_bites/presentation/screens/Forms/requests_form.dart';
 import 'package:home_bites/presentation/screens/Home/Components/request_card.dart';
 import 'package:home_bites/services/pocketbase/pbase.dart';
 import 'package:home_bites/services/pocketbase/requests_stream.dart';
@@ -11,40 +10,23 @@ class RequestsTab extends StatefulWidget {
   const RequestsTab({super.key});
 
   @override
-  _RequestsTabState createState() => _RequestsTabState();
+  State<RequestsTab> createState() => _RequestsTabState();
 }
 
 class _RequestsTabState extends State<RequestsTab> {
   late RequestsStreamController streamController;
   late StreamSubscription<List<RecordModel>> subscription;
   List<RecordModel> requests = [];
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    if (index == 0) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => RequestsForm()),
-      );
-    } else {
-      setState(() {
-        _selectedIndex = index;
-      });
-    }
-  }
 
   @override
   void initState() {
     super.initState();
-
-    // Get PocketBaseService from the provider
     final PocketBaseService pbClient =
         Provider.of<PocketBaseService>(context, listen: false);
 
-    // Initialize the stream controller
-    streamController = RequestsStreamController(pb: pbClient.pb);
+    streamController = RequestsStreamController(
+        pb: pbClient.pb, myId: pbClient.pb.authStore.record!.id);
 
-    // Subscribe to the stream
     subscription = streamController.requestsStream.listen(
       (records) {
         setState(() {
@@ -59,7 +41,6 @@ class _RequestsTabState extends State<RequestsTab> {
 
   @override
   void dispose() {
-    // Cancel the subscription and dispose of the stream controller
     subscription.cancel();
     streamController.dispose();
     super.dispose();
@@ -75,42 +56,6 @@ class _RequestsTabState extends State<RequestsTab> {
             itemBuilder: (context, index) {
               return ReqCard(request: requests[index]);
             },
-          ),
-        ),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 180,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: BottomNavigationBar(
-                  items: const <BottomNavigationBarItem>[
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.add),
-                      label: 'Add',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.list),
-                      label: 'View',
-                    ),
-                  ],
-                  currentIndex: _selectedIndex,
-                  onTap: _onItemTapped,
-                ),
-              ),
-            ),
           ),
         ),
       ],
