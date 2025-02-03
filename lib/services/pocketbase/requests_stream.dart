b96e78618545be3784f2b879e084eaa85db9f4cf
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-02-03 21:00:22 +0530

    fix: bug raising error {bad state: cannot add event after closing}

--- lib/services/pocketbase/requests_stream.dart
+++ lib/services/pocketbase/requests_stream.dart
@@ -4,63 +4,72 @@ import 'package:pocketbase/pocketbase.dart';
 class RequestsStreamController {
   final PocketBase pb;
   late final StreamController<List<RecordModel>> _controller;
+  final String myId;
   List<RecordModel> _currentRequests = [];
   UnsubscribeFunc? _unsubscribe;
 
-  RequestsStreamController({required this.pb}) {
+  RequestsStreamController({required this.pb, required this.myId}) {
     _controller = StreamController<List<RecordModel>>(onCancel: dispose);
-    _loadInitialData();
-    _subscribeToChanges();
+    _loadInitialData(myId);
+    _subscribeToChanges(myId);
   }
 
   Stream<List<RecordModel>> get requestsStream => _controller.stream;
 
-  Future<void> _loadInitialData() async {
+  Future<void> _loadInitialData(String myId) async {
     try {
       _currentRequests = await pb.collection('requests').getFullList(
             sort: '-created',
             expand: 'requested_user',
+            filter: "requested_user!='$myId'",
           );
-      _controller.add(_currentRequests);
+      if (!_controller.isClosed) {
+        _controller.add(_currentRequests);
+      }
     } catch (e) {
       print('Error loading initial data: $e');
-      _controller.addError(e);
+      if (!_controller.isClosed) {
+        _controller.addError(e);
+      }
     }
   }
 
-  Future<void> _subscribeToChanges() async {
+  Future<void> _subscribeToChanges(String myId) async {
     try {
       _unsubscribe = await pb.collection('requests').subscribe(
         expand: 'requested_user',
+        filter: "requested_user!='$myId'",
         '*',
         (e) {
-          switch (e.action) {
-            case 'create':
-              final newRecord = RecordModel.fromJson(e.record!.data);
-              _currentRequests.add(newRecord);
-              _controller.add(_currentRequests.reversed.toList());
-              break;
-            case 'delete':
-              final deletedRecordId = e.record!.id;
-              _currentRequests
-                  .removeWhere((record) => record.id == deletedRecordId);
-              _controller.add(_currentRequests.reversed.toList());
-              break;
+          if (!_controller.isClosed) {
+            switch (e.action) {
+              case 'create':
+                final newRecord = RecordModel.fromJson(e.record!.data);
+                _currentRequests.add(newRecord);
+                _controller.sink.add(_currentRequests.reversed.toList());
+                break;
+              case 'delete':
+                final deletedRecordId = e.record!.id;
+                _currentRequests
+                    .removeWhere((record) => record.id == deletedRecordId);
+                _controller.sink.add(_currentRequests.reversed.toList());
+                break;
+            }
           }
         },
       );
     } catch (e) {
       print('Error subscribing to changes: $e');
-      _controller.addError(e);
+      if (!_controller.isClosed) {
+        _controller.addError(e);
+      }
     }
   }
 
-  // Method to manually refresh the data
   Future<void> refreshData() async {
-    await _loadInitialData();
+    await _loadInitialData(myId);
   }
 
-  // Don't forget to dispose
   void dispose() {
     _unsubscribe?.call();
     _controller.close();
